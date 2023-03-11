4d6ae3fac6ae7c46f606d8685b3b127e84a242cd
pattabhik <pattabhiramireddy.k@gmail.com>
2023-03-11 20:09:30 +0000

    changes committed

--- src/main/java/com/practice/spring/azure/demo/controller/DemoController.java
+++ src/main/java/com/practice/spring/azure/demo/controller/DemoController.java
@@ -1,12 +1,12 @@
 package com.practice.spring.azure.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	@RequestMapping("/get-msg")
+	@GetMapping("/get-msg")
 	public String getMessage() {
 		return "Welcome Spring Azure Demo Project!!";
 	}
