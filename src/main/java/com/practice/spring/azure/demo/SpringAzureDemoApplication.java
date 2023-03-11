a47bdfe6bf6afe58f393a9ee3627c3a8aac3ca93
pattabhik <pattabhiramireddy.k@gmail.com>
2023-03-11 19:37:58 +0000

    first commit

---
+++ src/main/java/com/practice/spring/azure/demo/SpringAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/spring/azure/demo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
